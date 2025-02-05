1ccc1bda6e64f7a6707a45038b50c38ea8472f65
Kap_God <kavinprasath2000@gmail.com>
2025-02-05 22:47:02 +0530

    first commit

---
+++ src/main/java/com/example/real/time/notification_System/RealTimeNotificationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.real.time.notification_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeNotificationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeNotificationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/real/time/notification_System/RealTimeNotificationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.real.time.notification_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeNotificationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
